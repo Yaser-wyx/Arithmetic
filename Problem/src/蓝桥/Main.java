c3f5d9e3ba1f808f4e9b68cef13f84de65def78a
wanyu <335767798@qq.com>
2018-04-09 15:27:00 +0800

    noip

--- Problem/src/蓝桥/Main.java
+++ Problem/src/蓝桥/Main.java
@@ -1,34 +1,44 @@
 package 蓝桥;
 
-import java.util.Scanner;
+import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int add = scanner.nextInt();
-        int nums[] = new int[n];
+    public static void main(String args[]) throws IOException {
+        StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(System.out));
+        in.nextToken();
+        int n = (int) in.nval;
+        int a[] = new int[n];
+        int b[] = new int[n];
+        in.nextToken();
+        int emperor = (int) in.nval;
+        in.nextToken();//丢弃皇帝右手的
+        BigInteger all = new BigInteger(Integer.toString(emperor));
+        BigInteger temp;
         for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
+            in.nextToken();
+            a[i] = (int) in.nval;
+            in.nextToken();
+            b[i] = (int) in.nval;
+            temp = new BigInteger(Integer.toString(a[i]));
+            all = all.multiply(temp);
         }
-        for (int i = 1; i <= add; i++) {
-            int k;
-            for (k = n - 1; k >= 0 && nums[k - 1] > nums[k]; k--) ;  //定位第一个不可以增加的数
-            k--;//最后一个可以增加的数
-            int find = nums[k] + 1;
-            int k2;
-            for (k2 = n - 1; k2 > k; k2--) {
-                if (nums[k2] == find) {
-                    break;
-                }
+        BigInteger ans = new BigInteger(all.toString());
+        for (int i = 0; i < n; i++) {
+            BigInteger temp_a = new BigInteger(Integer.toString(a[i]));
+            BigInteger temp_b = new BigInteger(Integer.toString(b[i]));
+            temp = all.divide(temp_a.multiply(temp_b));
+            if (ans.compareTo(temp) > 0) {
+                ans = temp;
             }
-            int temp = nums[k2];
-            nums[k2] = nums[k];
-            nums[k] = temp;//交换
-
-
-
         }
+        if (ans.compareTo(new BigInteger("0")) == 0) {
+            ans = new BigInteger("1");
+        }
+        printWriter.println(ans);
+        printWriter.flush();
+
 
     }
 }
